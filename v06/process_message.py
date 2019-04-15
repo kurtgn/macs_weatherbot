d081454d3cba88a8ce6b50db690ca10f7f65a7af
Mike <mikhail.g.novikov@gmail.com>
2019-04-15 20:57:01 +0300

    fix

--- v06/process_message.py
+++ v06/process_message.py
@@ -17,16 +17,13 @@ def process_chat_message(msg):
         waiting_for_cities = True
 
     elif text == 'Узнать погоду':
-
         reply = 'Узнаю погоду'
 
     else:
-
         if waiting_for_cities == True:
             reply = 'Устанавливаю город ' + text
             waiting_for_cities = False
         else:
-
             reply = 'Я еще совсем маленький и не понимаю человеческую речь. Воспользуйся кнопками, пожалуйста.'
 
     buttons = [['Установить город', 'Узнать погоду']]
