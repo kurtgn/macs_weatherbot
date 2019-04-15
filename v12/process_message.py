54e8459f6be17c03651519ce3c85063bcc648fcb
Mike <mikhail.g.novikov@gmail.com>
2019-04-15 21:34:53 +0300

    fix

--- v12/process_message.py
+++ v12/process_message.py
@@ -1,5 +1,9 @@
 from weather import get_weather
 from cities_db_manager import db, City
+
+
+
+
 # словарь, в котором ключи будут юзернеймами, а значения - True/False
 waiting_for_cities = {}
 cities = {}
@@ -8,30 +12,32 @@ cities = {}
 
 def process_chat_message(msg):
     text = msg['text']
-    chat_id = str(msg['from']['username'])
-    print(chat_id + ': ' + text)
+    username = msg['from']['username']
+    print(username + ': ' + text)
 
     if text == 'Установить город':
         reply = 'Отправь мне свой город'
-        waiting_for_cities[chat_id] = True
+        waiting_for_cities[username] = True
 
     elif text == 'Узнать погоду':
 
-        # если бот уже запомнил город человека - узнаем погоду в этом городе.
-        # иначе просим установить город
-        if chat_id in cities:
-            city = cities[chat_id]
-            reply = get_weather(city)
+        # пытаемся достать город из базы данных
+        city_from_db = City.query.filter_by(username=username).first()
+
+        # если он есть - достаем имя и запрашиваем погоду
+        if city_from_db:
+            city_name = city_from_db.city_name
+            reply = get_weather(city_name)
         else:
             reply = 'Сначала установи город.'
 
     else:
         # если бот ждет от пользователя город - то сохраняем пользовательский ввод.
         # иначе говорим, что не поняли пользователя
-        if chat_id in waiting_for_cities and waiting_for_cities[chat_id] == True:
+        if username in waiting_for_cities and waiting_for_cities[username] == True:
 
             # пытаемся достать город из базы данных
-            city_from_db = City.query.filter_by(username=chat_id).first()
+            city_from_db = City.query.filter_by(username=username).first()
 
             # если он есть - меняем имя города на новое (которое передал юзер)
             if city_from_db:
@@ -45,7 +51,7 @@ def process_chat_message(msg):
             db.session.add(city_from_db)
             db.session.commit()
 
-            waiting_for_cities[chat_id] = False
+            waiting_for_cities[username] = False
 
             reply = 'Устанавливаю город ' + text
         else:
